1724512e094df7a12a0097c0a02c4f7e7b98e49e
AnErrupTion <anerruption@disroot.org>
2024-08-09 23:56:44 +0200

    Only map needed addresses
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

--- src/paging.zig
+++ src/paging.zig
@@ -4,10 +4,8 @@ const TABLE_ENTRIES: u64 = 512;
 const ENTRY_SIZE: u64 = 8; // In bytes
 const TABLE_SIZE = TABLE_ENTRIES * ENTRY_SIZE; // In bytes
 
-const MAX_4GIB_ADDRESS = 0x100000000;
-
-const PRESENT_BIT: u64 = 1 << 0;
-const READ_WRITE_BIT: u64 = 1 << 1;
+pub const PRESENT_BIT: u64 = 1 << 0;
+pub const READ_WRITE_BIT: u64 = 1 << 1;
 
 const VirtualAddress = packed struct {
     sign_extension: u16,
@@ -30,13 +28,15 @@ var initial_hhdm_offset: u64 = undefined;
 var initial_kernel_physical_base: u64 = undefined;
 var initial_kernel_virtual_base: u64 = undefined;
 
+var pml4_table: [*]volatile u64 = undefined;
+
 pub fn init(hhdm_offset: u64, kernel_physical_base: u64, kernel_virtual_base: u64) error{OutOfMemory}!void {
     initial_hhdm_offset = hhdm_offset;
     initial_kernel_physical_base = kernel_physical_base;
     initial_kernel_virtual_base = kernel_virtual_base;
 
     const pml4_table_address = try pmm.allocate(TABLE_SIZE);
-    const pml4_table: [*]volatile u64 = @ptrFromInt(hhdm_offset + pml4_table_address);
+    pml4_table = @ptrFromInt(hhdm_offset + pml4_table_address);
 
     // Initialize the PML4 table with zeroes to make sure the present bit isn't set anywhere
     @memset(pml4_table[0..TABLE_ENTRIES], 0);
@@ -47,17 +47,9 @@ pub fn init(hhdm_offset: u64, kernel_physical_base: u64, kernel_virtual_base: u6
     // Similarly, those addresses will be the virtual addresses to where the sections will be mapped. We then use Limine's
     // Kernel Address Feature to get the physical address of each of those sections.
 
-    for (@intFromPtr(&TEXT_START)..@intFromPtr(&TEXT_END)) |i| try map(pml4_table, getAlignedKernelAddress(i), @bitCast(i), PRESENT_BIT | READ_WRITE_BIT);
-    for (@intFromPtr(&RODATA_START)..@intFromPtr(&RODATA_END)) |i| try map(pml4_table, getAlignedKernelAddress(i), @bitCast(i), PRESENT_BIT);
-    for (@intFromPtr(&DATA_START)..@intFromPtr(&DATA_END)) |i| try map(pml4_table, getAlignedKernelAddress(i), @bitCast(i), PRESENT_BIT | READ_WRITE_BIT);
-
-    // The system may have less than 4 GiB of memory, so we account for that
-    const max_address = @min(pmm.total_byte_count, MAX_4GIB_ADDRESS);
-
-    var address: u64 = 0;
-    while (address < max_address) : (address += pmm.PAGE_SIZE) {
-        try map(pml4_table, address, @bitCast(hhdm_offset + address), PRESENT_BIT | READ_WRITE_BIT);
-    }
+    for (@intFromPtr(&TEXT_START)..@intFromPtr(&TEXT_END)) |i| try map(getAlignedKernelAddress(i), @bitCast(i), PRESENT_BIT | READ_WRITE_BIT);
+    for (@intFromPtr(&RODATA_START)..@intFromPtr(&RODATA_END)) |i| try map(getAlignedKernelAddress(i), @bitCast(i), PRESENT_BIT);
+    for (@intFromPtr(&DATA_START)..@intFromPtr(&DATA_END)) |i| try map(getAlignedKernelAddress(i), @bitCast(i), PRESENT_BIT | READ_WRITE_BIT);
 
     // Enable paging
     asm volatile ("mov %cr3, %[value]"
@@ -66,7 +58,7 @@ pub fn init(hhdm_offset: u64, kernel_physical_base: u64, kernel_virtual_base: u6
     );
 }
 
-fn map(pml4_table: [*]volatile u64, physical_address: u64, virtual_address: VirtualAddress, flags: u64) error{OutOfMemory}!void {
+fn map(physical_address: u64, virtual_address: VirtualAddress, flags: u64) error{OutOfMemory}!void {
     const pdpr_entries = try getEntryOrAllocate(virtual_address.pml4_index, pml4_table, flags);
     const pd_entries = try getEntryOrAllocate(virtual_address.pdpr_index, pdpr_entries, flags);
     const pt_entries = try getEntryOrAllocate(virtual_address.pd_index, pd_entries, flags);
--- src/vmm.zig
+++ src/vmm.zig
@@ -1,4 +1,5 @@
 const pmm = @import("pmm.zig");
+const paging = @import("paging.zig");
 
 var paging_hhdm_offset: u64 = undefined;
 
@@ -7,7 +8,12 @@ pub fn init(hhdm_offset: u64) void {
 }
 
 pub fn allocate(size: u64) error{OutOfMemory}!u64 {
-    return paging_hhdm_offset + try pmm.allocate(size);
+    const physical_address = try pmm.allocate(size);
+    const virtual_address = paging_hhdm_offset + physical_address;
+
+    try paging.map(physical_address, @bitCast(virtual_address), paging.PRESENT_BIT | paging.READ_WRITE_BIT);
+
+    return virtual_address;
 }
 
 pub fn free(address: u64, size: u64) void {
